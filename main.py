6514cd5a18b9ef53e397c9d0609f4d7cde47fc3f
sameh farouk <sfarouk702@gmail.com>
2024-04-30 20:30:43 +0300

    update main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+from routes import base
+
 
 app = FastAPI()
 
-@app.get('/we')
-def welcome():
-    return {
-        "masseage":"welcome ya pro"
-    }
+app.include_router(base.base_route)
+
